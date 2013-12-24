50ad26342622eb4e9ea68624ecffd31469549cd1
sbeslow <bezzy21@yahoo.com>
2013-12-23 22:32:45 -0600

    Moved over the scraper loop

--- Scraper/src/com/hydrophilik/mwrdCsoScraper/executables/RunScraper.java
+++ Scraper/src/com/hydrophilik/mwrdCsoScraper/executables/RunScraper.java
@@ -1,6 +1,13 @@
 package com.hydrophilik.mwrdCsoScraper.executables;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
 import com.orangewall.bezutils.beztime.BezDate;
 
@@ -24,7 +31,7 @@ public class RunScraper {
 		BezDate endDate;
 		
 		try {
-			theDate = new BezDate("11/1/2012");
+			theDate = new BezDate("12/22/2013");
 			endDate = new BezDate(); // today
 		}
 		catch (Exception e) {
@@ -35,6 +42,12 @@ public class RunScraper {
 		System.out.println("Scraper started with start date: " + theDate.convertToString() +
 				" and end date: " + endDate.convertToString());
 		
+		scrapeWebsite(theDate, endDate, rawScrapingsDir);
+
+	}
+	
+	private static void scrapeWebsite(BezDate theDate, BezDate endDate, String rawScrapingsDir) {	
+		
 		while (false == theDate.isSameDayAs(endDate)) {
 			
 			File file = new File(rawScrapingsDir + "/" + theDate.dateYearFirst() + ".txt");
@@ -42,6 +55,41 @@ public class RunScraper {
 			if (file.exists()) {
 				// Date has already been scraped.  Skip it.
 				return;
+			}
+			
+			String date = theDate.convertToString();
+			
+			BufferedWriter bw = null;
+			FileWriter fw = null;
+			
+			try {
+			
+				file.createNewFile();
+			
+				String inputLine;
+				String urlStr = mwrdWebPrefix + date;
+				URL url = new URL(urlStr);
+				URLConnection yc = url.openConnection();
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+	                                    yc.getInputStream()));
+	
+				fw = new FileWriter(file.getAbsoluteFile());
+				bw = new BufferedWriter(fw);
+				while ((inputLine = in.readLine()) != null)
+					bw.write(inputLine + "\n");
+			}
+			catch (Exception e) {
+				e.printStackTrace();
+				return;
+			}
+			finally {
+				try {
+					bw.close();
+					fw.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+					return;
+				}
 			}
 			
 			System.out.println("Processing: " + file.getAbsolutePath());			
