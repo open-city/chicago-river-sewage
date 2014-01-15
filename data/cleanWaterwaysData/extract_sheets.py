af00337098d09541709dd4e8c1e489ca3fc12f2b
James Courtois <jcourtois@thoughtworks.com>
2014-01-15 12:11:25 -0600

    james | added xlrd to the requirements.txt file; refactored extract_sheets.py

--- data/cleanWaterwaysData/extract_sheets.py
+++ data/cleanWaterwaysData/extract_sheets.py
@@ -1,24 +1,25 @@
 from mmap import mmap,ACCESS_READ
-from xlrd import open_workbook,xldate_as_tuple
+from xlrd import open_workbook, xldate_as_tuple
 import datetime
 import csv
 
+def process(cell):
+    if cell.ctype == 3:
+        value = xldate_as_tuple(cell.value, wb.datemode)
+        value = str(datetime.datetime(*value))
+    else:
+        value = unicode(cell.value).encode("utf-8")
+    return value
+
+def csv_filename_from(sheet, year):
+    return str(year) + '/' + sheet.name.replace(" ", "").lower() + '.csv'
+
 for year in range(2005,2013):
-  wb = open_workbook(str(year) + '/waterways' + str(year) + '.xls')
-  for s in wb.sheets():
-    if str(year) in s.name :	
-      print 'Sheet:',s.name
-      with open(str(year) + '/' + s.name.replace(" ", "") + '.csv', 'w') as f :
-        writer = csv.writer(f)
-        for row in range(s.nrows):
-          values = []
-          for col in range(s.ncols):
-            value = ''
-            cell = s.cell(row,col)
-            if cell.ctype == 3:
-              value = xldate_as_tuple(cell.value,wb.datemode)
-              value = str(datetime.datetime(*value))
-            else:
-              value = unicode(cell.value).encode("utf-8")
-            values.append(value)
-          writer.writerow(values)
+    wb = open_workbook(str(year) + '/waterways' + str(year) + '.xls')
+    annual_water_sheets = [sheet for sheet in wb.sheets() if str(year) in sheet.name]
+    for s in annual_water_sheets:
+        with open(csv_filename_from(s, year), 'w') as f :
+            writer = csv.writer(f)
+            rows = [[process(s.cell(row,col)) for col in range(s.ncols)] for row in range(s.nrows)]
+            writer.writerows(rows)
+
