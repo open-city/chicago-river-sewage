92382b5c25292c6ea8353646a9a04c4b21165997
James Courtois <jcourtois@thoughtworks.com>
2014-01-23 20:14:24 -0600

    James | Cleaned up the formatting in lat/long in clean-waterway-measurements.csv.  Measurements are now in floating point degrees

--- data/clean-waterway/02_extract_sheets.py
+++ data/clean-waterway/02_extract_sheets.py
@@ -3,6 +3,7 @@ from xlrd import open_workbook, xldate_as_tuple
 import datetime
 import csv
 import re
+import IPython as py
 
 # useful for cleaning up the headers; 'pH   ' => 'ph'
 def clean_string(value):
@@ -40,23 +41,9 @@ def csv_filename_from(sheet, year):
 def waterways_workbook_for(year):
     return str(year) + '/waterways' + str(year) + '.xls'
 
-def process_location_sheet(rows):
-    rows.pop(1)
-    rows[0] = [ cell.lower().replace('\n', '_') for cell in rows[0] ]
-    rows[0][0] = 'sampling(point)'
-    return rows
-
 for year in range(2005,2013):
     wb = open_workbook(waterways_workbook_for(year))
     annual_water_sheets = [sheet for sheet in wb.sheets() if str(year) in sheet.name]
-    location_sheet = [sheet for sheet in wb.sheets() if 'location' in sheet.name.lower()]
-    for s in location_sheet:
-        with open(csv_filename_from(s, year), 'w') as f:
-            writer = csv.writer(f)
-            sheet = [[process_cell(s.cell(row,col)) for col in range(s.ncols)] for row in range(s.nrows)]
-            sheet = process_location_sheet(sheet)
-            writer.writerows(sheet)
-
     for s in annual_water_sheets:
         with open(csv_filename_from(s, year), 'w') as f :
             writer = csv.writer(f)
--- data/clean-waterway/03_flattening.py
+++ data/clean-waterway/03_flattening.py
@@ -8,17 +8,17 @@ months = [calendar.month_name[i].lower() for i in range(1, 13)]
 years = range(2005, 2013)
 
 for year in years:
-    location_path = '%s/waterwayslocations.csv' % year
-    location_frame = pd.read_csv(location_path)
     for month in months:
         path = '%s/%s%s.csv' % (year, month, year)
         frame = pd.read_csv(path)
-        frame = frame.merge(location_frame, how='left', on='sampling(point)')
         columns_to_remove = (x for x in frame.columns if (re.search('^\(\)\.\d+$|^\(\)$|^$|Unnamed|code',x))) # Remove columns that don't have headers, can't join correctly or really tell what the data is
         for i in columns_to_remove:
             frame = frame.drop(i,1)
         pieces.append(frame)
         print('%s/%s%s.csv' % (year, month, year))
 flat = pd.concat(pieces, ignore_index=True, join='outer')
+
+location_frame = pd.read_csv('waterwayslocations.csv')
+flat = flat.merge(location_frame, how='left', on='sampling(point)')
 
 flat.to_csv("clean-waterway-measurements.csv")
