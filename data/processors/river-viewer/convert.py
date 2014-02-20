f4146737f9e6065d650b6187deb199d608ab2a85
Forest Gregg <fgregg@uchicago.edu>
2014-02-20 15:33:11 -0600

    extract layers

--- data/processors/river-viewer/convert.py
+++ data/processors/river-viewer/convert.py
@@ -1,11 +1,21 @@
 import json
 import esri_to_geo
+import subprocess
 
 with open('arcgis_layers.json', 'r') as f :
     arcgis_layers = json.load(f)
 
 for layer in arcgis_layers["operationalLayers"] :
-    if layer["id"] == 'cso_chicago_caws_active_4354' :
-        esri_json = layer["featureCollection"]["layers"][0]["featureSet"]
-        with open('cso_outfalls_state_plane.geojson', 'w') as f :
-            json.dump(esri_to_geo.esri_to_geo(esri_json), f)
+    layer_name = layer["featureCollection"]["layers"][0]["layerDefinition"]["name"]
+
+    esri_json = layer["featureCollection"]["layers"][0]["featureSet"]
+    file_name = layer_name + '_pseudo_mercator.geojson'
+
+    with open(file_name, 'w') as f :
+        json.dump(esri_to_geo.esri_to_geo(esri_json), f)
+
+    command = 'ogr2ogr -f "GeoJSON" %s.geojson %s -s_srs EPSG:3857 -t_srs EPSG:4326' % (layer_name, file_name)
+    print command
+    subprocess.call(command, shell=True)
+
+
