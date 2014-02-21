6c9139bdbf7e1e195fc5143c4319ce7cb830f4fd
Gong-Yi Liao <gongyi.liao@gmail.com>
2014-02-20 20:42:16 -0600

    add new columns to determine the value is a lower bound or not

--- analysis/gong-yi/find_nd_vals.R
+++ analysis/gong-yi/find_nd_vals.R
@@ -22,7 +22,7 @@ library(plyr)
 sew.dat <- read.csv("../../data/processed_data/clean-waterway-measurements.csv",
                     stringsAsFactors=FALSE)
 
-## get observations with "<"
+## get observations with "<" or something non-numeric values 
 get.df.nnumv <- function(x.df) {
     get.col.nnumv <- function(x) {
         y <- unique(x)
@@ -31,11 +31,33 @@ get.df.nnumv <- function(x.df) {
     return(apply(x.df, 2, get.col.nnumv))
 }
 
-##
+## get lower bound values
+get.lb.val <- function(x.col) {
+    x.col.vals <- x.col
+    x.col.is.lb <- rep(FALSE, length(x.col.vals))
+    x.lb.idx <- grep("<", x.col.vals)
+    x.col.is.lb[x.lb.idx]<- TRUE
+    x.lb.vals <- x.col[x.lb.idx]
+    x.lb.vals <- gsub("<", "", x.lb.vals)
+    x.col.vals[x.lb.idx] <- x.lb.vals
+    x.col.vals <- as.numeric(x.col.vals)
+    x.col.annoted <- data.frame(x=x.col.vals,
+                                 is.lb=x.col.is.lb)
+    return(x.col.annoted)
+}
+    
+## process numeric column first 
 sew.dat.numv <- sew.dat
+
+## these columns are already known to be non-numeric 
 sew.dat.numv$collect.date. <- NULL
-sew.dat.numv$location.description <- NULL
-sew.dat.problematic.vals <- get.df.nnumv(sew.dat)
+sew.dat.numv$location_description <- NULL
+
+## get the problematic values in numeric columns, put them into a list 
+sew.dat.problematic.vals <- get.df.nnumv(sew.dat.numv)
+
+
+
 
 ## -----------------------------------------------------------------------------
 ##
