26a9dc5a9c70b1642bb893a1de9102ebf03b4611
Eric van Zanten <eric.vanzanten@gmail.com>
2015-06-17 14:13:57 +0000

    scraped data for 06/16/2015

--- data/scrapers/Scraper/run_scraper.sh
+++ data/scrapers/Scraper/run_scraper.sh
@@ -1,7 +1,7 @@
 START_DATE=`/bin/date +%m/%d/%Y -d "1 month ago"`
 END_DATE=`/bin/date +%m/%d/%Y -d "1 day ago"`
 
-java -jar data/scrapers/Scraper/classes/artifacts/Scraper_jar/Scraper.jar ${START_DATE} ${END_DATE} data/processed_data/cso-data.db
+java -jar classes/artifacts/Scraper_jar/Scraper.jar ${START_DATE} ${END_DATE} ../../processed_data/cso-data.db
 git add .
 git commit -m "scraped data for ${END_DATE}"
 git push origin master
