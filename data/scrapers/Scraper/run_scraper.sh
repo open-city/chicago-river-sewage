a637cdd9cead08504d78742dbc272e13c574b690
Eric van Zanten <eric.vanzanten@gmail.com>
2015-07-21 22:06:26 +0000

    update to path for adding sqlite db

--- data/scrapers/Scraper/run_scraper.sh
+++ data/scrapers/Scraper/run_scraper.sh
@@ -2,7 +2,7 @@ START_DATE=`/bin/date +%m/%d/%Y -d "1 month ago"`
 END_DATE=`/bin/date +%m/%d/%Y -d "1 day ago"`
 
 java -jar classes/artifacts/Scraper_jar/Scraper.jar ${START_DATE} ${END_DATE} ../../processed_data/cso-data.db
-git add .
+git add ../../processed_data/cso-data.db
 git commit -m "scraped data for ${END_DATE}"
 git push origin master
 git push heroku master
