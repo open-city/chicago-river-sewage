602470d3e119016d76f54bb7f5233b191e210178
Patrick Sier <Sier-Patrick@norc.org>
2016-08-31 16:36:54 -0500

    Fixed author, added sqlite db update, checking for existing records

--- data/scrapers/updated_mwrd_scraper/mwrd_scraper.py
+++ data/scrapers/updated_mwrd_scraper/mwrd_scraper.py
@@ -4,6 +4,11 @@ import csv
 from datetime import datetime, timedelta
 import requests
 from bs4 import BeautifulSoup
+import os
+import sqlite3
+
+data_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+processed_data_dir = os.path.join(data_dir, 'processed_data')
 
 mwrd_url = 'http://apps.mwrd.org/csoreports/CSO_Synopisis_Report'
 headers = {
@@ -19,6 +24,34 @@ drop_keys = [
     'ReportViewer1$ToggleParam$img'
 ]
 payload = dict()
+
+# Mapping of segment names to segment integer IDs
+UPDATED_SEGMENTS = {
+    "NSC Upper (NSWRP)": 1,
+    "NSC Lower (NSWRP)": 2,
+    "NBCR Lower (NSC Confluence)": 3,
+    "NBCR Upper (NSC Confluence)": 4,
+    "Chicago R": 5,
+    "SB Chicago R": 6,
+    "SF SB Chicago R": 7,
+    "CSSC Upper (SWRP)": 8,
+    "CSSC Lower (SWRP)": 9,
+    # Default for CSSC Lower (SWRP) is 9, rules checking for other values (10, 11)
+    "Weller Cr": 12,
+    "DesPlaines Upper": 13,
+    "DesPlaines Middle": 14,
+    "DesPlaines Lower": 15,
+    "Salt Cr": 16,
+    "Cal R": 17,
+    "Grand Cal R": 18,
+    "Little Cal R (North)": 19,
+    "Little Cal R (South)": 20,
+    "Cal Sag Ch": 21,
+    "Cal Union drainage Ditch": 22,
+    "Addison Cr": 23
+    # 30, 31, and 32 follow different rules, not sure about 31 overall
+    # Haven't seen any examples for 32 O'Brien
+}
 
 def scrape_table(start_date, end_date):
     s = requests.Session()
@@ -49,7 +82,6 @@ def scrape_table(start_date, end_date):
                                        headers=headers,
                                        cookies=s.cookies)
     mwrd_post_soup = BeautifulSoup(mwrd_post_response.content, 'html.parser')
-    #print(mwrd_post_soup)
 
     # MWRD markup is not well formatted, there are many nested tables, so this
     # is the best CSS selector so far
@@ -67,36 +99,33 @@ def scrape_table(start_date, end_date):
         table_list.append(row_list)
 
     mod_filename = date_today.replace('/', '-')
+    mwrd_filepath = os.path.join
     with open('data/mwrd_scraper_{}.csv'.format(mod_filename), 'w') as mwrd_file:
         mwrd_writer = csv.writer(mwrd_file, delimiter=',', lineterminator='\n')
         for row in table_list:
             mwrd_writer.writerow(row)
 
-UPDATED_SEGMENTS = {
-    "NSC Upper (NSWRP)": 1,
-    "NSC Lower (NSWRP)": 2,
-    "NBCR Lower (NSC Confluence)": 3,
-    "NBCR Upper (NSC Confluence)": 4,
-    "Chicago R": 5,
-    "SB Chicago R": 6,
-    "SF SB Chicago R": 7,
-    "CSSC Upper (SWRP)": 8,
-    "CSSC Lower (SWRP)": 9,
-    # Default for CSSC Lower (SWRP) is 9, rules checking for other values (10, 11)
-    "Weller Cr": 12,
-    "DesPlaines Upper": 13,
-    "DesPlaines Middle": 14,
-    "DesPlaines Lower": 15,
-    "Salt Cr": 16,
-    "Cal R": 17,
-    "Grand Cal R": 18,
-    "Little Cal R (North)": 19,
-    "Little Cal R (South)": 20,
-    "Cal Sag Ch": 21,
-    "Cal Union drainage Ditch": 22,
-    "Addison Cr": 23
-    # 30, 31, and 32 follow different rules, not sure about 31 overall, and haven't seen any examples for 32 O'Brien
-}
+def write_to_db(updated_table):
+    con = sqlite3.connect(os.path.join(processed_data_dir, 'cso-data.db'))
+    cur = con.cursor()
+    insert_records = list()
+    check_records = [(i['location'],
+                      i['segment'],
+                      i['date'],
+                      i['starttime'],
+                      i['endtime'],
+                      i['duration']) for i in updated_table]
+
+    cur.execute("SELECT location, segment, date, starttime, endtime, duration FROM CSOs;")
+    results = cur.fetchall()
+    for r in check_records:
+        if r not in results:
+            insert_records.append(r)
+
+    cur.executemany("INSERT INTO CSOs (location, segment, date, starttime, endtime, duration) VALUES (?, ?, ?, ?, ?, ?);",
+                    insert_records)
+    con.commit()
+    con.close()
 
 # Updates CSV format to be compatible with existing database
 def reformat_compatible(mwrd_file):
@@ -133,6 +162,7 @@ def reformat_compatible(mwrd_file):
         updated_row['duration'] = int(duration_min.total_seconds() / 60)
         updated_mwrd_table.append(updated_row)
 
+    updated_mwrd_table.sort(key=lambda x: (x['date'], x['starttime']))
     mod_filename = date_today.replace('/', '-')
     with open('data/mwrd_scraper_compatible_{}.csv'.format(mod_filename), 'w') as mwrd_clean:
         mwrd_writer = csv.DictWriter(mwrd_clean,
@@ -142,9 +172,11 @@ def reformat_compatible(mwrd_file):
         mwrd_writer.writeheader()
         mwrd_writer.writerows(updated_mwrd_table)
 
+    write_to_db(updated_mwrd_table)
+
 if __name__ == '__main__':
     date_today = datetime.now().strftime("%m/%d/%Y")
-    #scrape_table(date_today, date_today)
-    scrape_table('01/01/2016', date_today)
+    # Start from first day after data collection stopped
+    scrape_table('05/15/2016', date_today)
     file_suffix = date_today.replace('/', '-')
     reformat_compatible('data/mwrd_scraper_{}.csv'.format(file_suffix))
